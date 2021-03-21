3d2cb2f8cd61f90c1f472461be16b325c0965ea6
Yummy275 <htgonzalez94@yahoo.com>
2021-03-20 23:04:25 -0700

    getCoinsInfo working onEffect in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import StdBtn from './components/StdBtn';
+import getCoinsInfo from './data/getCoinsInfo';
+
+const savedCoins = {
+    btc: { symbol: 'BTC', amount: '12.03' },
+    eth: { symbol: 'ETH', amount: '1' },
+};
+
+const getSavedCoinsSymbols = () => {
+    const symbols = [];
+    for (const coin in savedCoins) {
+        symbols.push(savedCoins[coin].symbol);
+    }
+    return symbols;
+};
 
 function App() {
-    return <StdBtn string="Options"></StdBtn>;
+    const [coinInfo, setCoinInfo] = useState([]);
+    console.log(coinInfo);
+    useEffect(() => {
+        const fetchData = async () => {
+            const savedCoinSymbols = getSavedCoinsSymbols();
+            const data = await getCoinsInfo(savedCoinSymbols);
+            setCoinInfo(data);
+        };
+        fetchData();
+    }, []);
+    return <div>hello</div>;
 }
 
 export default App;
---
+++ src/data/getCoinsInfo.js
@@ -0,0 +1,10 @@
+const getCoinsInfo = async (coins) => {
+    const coinNames = coins.join(',');
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const target = `https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&ids=${coinNames}&interval=1h,1d,30d`;
+    const response = await fetch(target);
+    const data = await response.json();
+    return data;
+};
+
+export default getCoinsInfo;
