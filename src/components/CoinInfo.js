5c2167ebcfe7ecafcc7ba827c83371a975140099
Yummy275 <htgonzalez94@yahoo.com>
2021-03-21 12:06:35 -0700

    qs coininfo component looking ok

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
 const styles = {
-    container: `bg-spaceCadet`,
+    container: `bg-spaceCadet w-5/6 p-2`,
+    headerHolder: `flex items-center space-x-1`,
+    coinLogo: 'w-10 h-10',
+    headingText: `text-xl`,
+    coinPriceInfoHolder: ``,
 };
 
 const CoinInfo = ({ logo, name, symbol, price, intervalPercentChange }) => {
+    console.log(price);
+    const parsedPrice = parseFloat(price);
+    const formattedPrice = parsedPrice.toFixed(2);
+
     return (
         <div className={styles.container}>
-            <img alt="Coin logo" src={logo}></img>
-            <h4>{name}</h4>
-            <h3>{symbol}</h3>
-            <h2>${price}</h2>
-            <p>{intervalPercentChange}</p>
+            <div className={styles.headerHolder}>
+                <h2 className={styles.heading}>
+                    {name} ({symbol})
+                </h2>
+                <img
+                    alt="Coin logo"
+                    src={logo}
+                    className={styles.coinLogo}
+                ></img>
+            </div>
+            <div className={styles.coinPriceInfoHolder}>
+                <p>Hold: 5</p>
+                <p>@ ${formattedPrice} per</p>
+                <p>$50,000</p>
+                <p>{intervalPercentChange}</p>
+            </div>
         </div>
     );
 };
