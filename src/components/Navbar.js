22208a21723dc0cd24061c84c1134587f1ab101f
Yummy275 <htgonzalez94@yahoo.com>
2021-04-05 16:41:43 -0700

    search box transition on focus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const styles = {
     navbar: `relative h-12 flex justify-center items-center bg-englishLavender`,
     searchInput: `h-3/4 rounded-md p-1 bg-white`,
-    searchBox: 'absolute h-24 w-56 top-12 bg-englishLavender',
+    searchBox:
+        'transition-all duration-1000 absolute w-60 top-12 bg-englishLavender',
 };
 
 const Navbar = () => {
+    const searchInput = useRef();
+    const [searchBoxHidden, setSearchBoxHidden] = useState(true);
+
     return (
         <div className={styles.navbar}>
-            <input className={styles.searchInput} placeholder="Search"></input>
-            <div className={styles.searchBox}></div>
+            <input
+                onFocus={() => setSearchBoxHidden(false)}
+                onBlur={() => setSearchBoxHidden(true)}
+                ref={searchInput}
+                className={styles.searchInput}
+                placeholder="Search"
+            ></input>
+            <div
+                className={`${styles.searchBox} ${
+                    searchBoxHidden ? 'h-0' : 'h-48'
+                }`}
+            ></div>
         </div>
     );
 };
