3d72db13f4c6b78fbacab9290ebfe5b3b813b9ef
Yummy275 <htgonzalez94@yahoo.com>
2021-08-23 11:36:27 -0700

    qs

--- src/components/AddCoinModal.js
+++ src/components/AddCoinModal.js
@@ -4,6 +4,8 @@ const styles = {
     blurredBg:
         'blurredBg flex justify-center items-center w-screen h-screen absolute z-10',
     contentContainer: 'w-102 h-102 p-3 bg-white rounded shadow-md',
+    title: 'text-center text-lg',
+    info: '',
 };
 
 const AddCoinModal = ({ coinData }) => {
@@ -11,7 +13,9 @@ const AddCoinModal = ({ coinData }) => {
     return (
         <div className={styles.blurredBg}>
             <div className={styles.contentContainer}>
-                <p>ADD COIN MODAL</p>
+                <div
+                    className={styles.title}
+                >{`${coinData.name} (${coinData.id})`}</div>
             </div>
         </div>
     );
